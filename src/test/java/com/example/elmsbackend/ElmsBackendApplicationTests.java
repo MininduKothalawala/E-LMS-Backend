f1796af208b1fde43b3564690d2edc1f0786a37d
hansitharaka <it19188850@my.sliit.lk>
2021-08-08 19:49:27 +0530

    Initial project setup

---
+++ src/test/java/com/example/elmsbackend/ElmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elmsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElmsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
