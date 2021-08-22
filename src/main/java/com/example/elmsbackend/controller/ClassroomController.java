13088ac3db22de95bebdd34bf62f51ff1eababe0
hansitharaka <it19188850@my.sliit.lk>
2021-08-22 20:31:23 +0530

    cross-origin url fixed

--- src/main/java/com/example/elmsbackend/controller/ClassroomController.java
+++ src/main/java/com/example/elmsbackend/controller/ClassroomController.java
@@ -11,7 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/classroom")
 public class ClassroomController {
 
